1bae743f8dd0aaf2a97e25cfabf1256e13003735
Abdulsalam Abdikarim <achalam00@gmail.com>
2023-11-26 23:56:21 +0300

    Progress made

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_numbers = list(0,)
+    fibonacci_numbers.append(1)
+    next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
+    for n in range(0, length):
+        # n = fibonacci_numbers[len(fibonacci_numbers)-2] + fibonacci_numbers[len(fibonacci_numbers)-1]
+        fibonacci_numbers.append(next_number)
+    return fibonacci_numbers
+
+print(print_fibonacci(10))
