635ef118a9c92b4aad97d67a25172b468fd109fe
Abdulsalam Abdikarim <achalam00@gmail.com>
2023-11-27 01:03:40 +0300

    Printing the numbers

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    fibonacci_numbers = list(0,)
-    fibonacci_numbers.append(1)
-    next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
-    for n in range(0, length):
-        # n = fibonacci_numbers[len(fibonacci_numbers)-2] + fibonacci_numbers[len(fibonacci_numbers)-1]
+    fibonacci_numbers = [0,1]
+    for number in range(0, length):
+        next_number = fibonacci_numbers[len(fibonacci_numbers)-2] + fibonacci_numbers[len(fibonacci_numbers)-1]
         fibonacci_numbers.append(next_number)
     return fibonacci_numbers
 
-print(print_fibonacci(10))
+print(print_fibonacci(7))
